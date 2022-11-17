3f3482d5c30adc5df3bcc1937d422c2fc00feead
Srirama Bhat <108423390+SriramaBhat@users.noreply.github.com>
2022-11-17 07:20:33 +0530

    Add files via upload

---
+++ customer.sql
@@ -0,0 +1,94 @@
+CREATE DATABASE IF NOT EXISTS customer_160;
+USE customer_160;
+CREATE TABLE IF NOT EXISTS customer (
+    cust INTEGER NOT NULL,
+    cname VARCHAR(50) NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    oid INTEGER NOT NULL,
+    odate DATE NOT NULL,
+    cust INTEGER,
+    order_amt INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS order_item (
+    orders INTEGER NOT NULL,
+    item INTEGER,
+    qty INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS item (
+    itemid INTEGER NOT NULL,
+    unitprice INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shipment (
+    orders INTEGER NOT NULL,
+    warehouse INTEGER,
+    ship_date DATE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS warehouse (
+    warehouseid INTEGER NOT NULL,
+    city VARCHAR(50) NOT NULL
+);
+
+INSERT INTO customer VALUES
+(0001, "Customer_1", "Mysuru"),
+(0002, "Customer_2", "Bengaluru"),
+(0003, "Customer_3", "Mumbai"),
+(0004, "Customer_4", "Dehli"),
+(0005, "Customer_5", "Bengaluru");
+
+INSERT INTO orders VALUES
+(001, "2020-01-14", 0001, 2000),
+(002, "2021-04-13", 0002, 500),
+(003, "2019-10-02", 0005, 2500),
+(004, "2019-05-12", 0003, 1000),
+(005, "2020-12-23", 0004, 1200);
+
+INSERT INTO item VALUES
+(0001, 400),
+(0002, 200),
+(0003, 1000),
+(0004, 100),
+(0005, 500);
+
+INSERT INTO warehouse VALUES
+(0001, "Mysuru"),
+(0002, "Bengaluru"),
+(0003, "Mumbai"),
+(0004, "Dehli"),
+(0005, "Chennai");
+
+INSERT INTO order_item VALUES 
+(001, 0001, 5),
+(002, 0005, 1),
+(003, 0005, 5),
+(004, 0003, 1),
+(005, 0004, 12);
+
+INSERT INTO shipment VALUES
+(001, 0002, "2020-01-16"),
+(002, 0001, "2021-04-14"),
+(003, 0004, "2019-10-07"),
+(004, 0003, "2019-05-16"),
+(005, 0005, "2020-12-23");
+
+ALTER TABLE orders MODIFY cust INTEGER NOT NULL;
+ALTER TABLE order_item MODIFY item INTEGER NOT NULL;
+ALTER TABLE shipment MODIFY warehouse INTEGER NOT NULL;
+
+UPDATE orders SET order_amt = 600 WHERE orderid = 002;
+UPDATE order_item SET item = 0002 WHERE orders = 002;
+UPDATE order_item SET qty = 3 WHERE orders = 002;
+DELETE FROM warehouse WHERE warehouseid = 0004;
+
+SELECT * FROM customer;
+SELECT * FROM orders;
+SELECT * FROM order_item;
+SELECT * FROM item;
+SELECT * FROM shipment;
+SELECT * FROM warehouse;
---
+++ enrollment.sql
@@ -0,0 +1,83 @@
+CREATE DATABASE IF NOT EXISTS enrollment_160;
+USE enrollment_160;
+CREATE TABLE IF NOT EXISTS student (
+    regno VARCHAR(20) NOT NULL,
+    sname VARCHAR(50) NOT NULL,
+    major VARCHAR(50) NOT NULL,
+    bdate DATE
+);
+
+CREATE TABLE IF NOT EXISTS course (
+    course INTEGER NOT NULL,
+    cname VARCHAR(50) NOT NULL,
+    dept VARCHAR(25) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS enroll (
+    regno VARCHAR(20),
+    course INTEGER NOT NULL,
+    sem INTEGER,
+    marks INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS book_adoption (
+    course INTEGER,
+    sem INTEGER,
+    bookISBN INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS text_book (
+    bookISBN INTEGER NOT NULL,
+    book_title VARCHAR(50),
+    publisher VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL
+);
+
+INSERT INTO student VALUES
+("01HF235", "Student_1", "Computer Engineering", "2001-05-15"),
+("01HF354", "Student_2", "Literature", "2002-06-10"),
+("01HF254", "Student_3", "Philosophy", "2000-04-04"),
+("01HF653", "Student_4", "History", "2003-10-12"),
+("01HF234", "Student_5", "Economics", "2001-10-10");
+
+INSERT INTO course VALUES
+(001, "Computer Engineering", "CS"),
+(002, "Literature", "English"),
+(003, "Philosophy", "Philosphy"),
+(004, "History", "Social Science"),
+(005, "Economics", "Social Science");
+
+INSERT INTO enroll VALUES
+("01HF235", 001, 5, 85),
+("01HF354", 002, 6, 87),
+("01HF254", 003, 3, 95),
+("01HF653", 004, 3, 80),
+("01HF234", 005, 5, 75);
+
+INSERT INTO book_adoption VALUES
+(001, 5, 241563),
+(002, 6, 532678),
+(003, 3, 453723),
+(004, 3, 278345),
+(001, 6, 426784);
+
+INSERT INTO text_book VALUES
+(241563, "Operating Systems", "McGraw Hill", "Silberschatz"),
+(532678, "Complete Works of Shakesphere", "Oxford", "Shakesphere"),
+(453723, "Immanuel Kant", "Delphi Classics", "Immanuel Kant"),
+(278345, "History of the world", "The Times", "Richard Overy"),
+(426784, "Behavioural Economics", "Hot Science", "David Orrel");
+
+ALTER TABLE enroll MODIFY regno VARCHAR(20) NOT NULL;
+ALTER TABLE text_book MODIFY book_title VARCHAR(50) NOT NULL;
+ALTER TABLE student MODIFY bdate DATE NOT NULL;
+
+INSERT INTO text_book VALUES (567385, "Principles of Cryptography", "McGraw Hill", "Silberschatz");
+UPDATE book_adoption SET bookISBN = 567385 WHERE course = 5;
+DELETE FROM text_book WHERE bookISBN = 241563;
+
+SELECT * FROM student;
+SELECT * FROM course;
+SELECT * FROM enroll;
+SELECT * FROM book_adoption;
+SELECT * FROM text_book;
